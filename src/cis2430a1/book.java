ef291d1f3d031b825ef4cbffd9898822648d8f41
dominickhera <dominickhera@192.168.0.30>
2016-11-07 12:00:02 -0500

    init commit

---
+++ src/cis2430a1/book.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cis2430a1;
+
+/**
+ *
+ * @author dominickhera
+ */
+public class book 
+{
+	private int productID;
+	private String name;
+	private String price;
+	private int year;
+	private String author;
+	private String publisher;
+
+	public book (int productID, String name, String price, int year, String author, String publisher)
+	{
+		this.productID = productID;
+		this.name = name;
+		this.price = price;
+		this.year = year;
+		this.author = author;
+		this.publisher = publisher;
+	}
+
+	public int sendBProdID()
+	{
+		return productID;
+	}
+
+	public String sendStringBProdID()
+	{
+		String stringBYear;
+
+		stringBYear = Integer.toString(year);
+		return stringBYear;
+	}
+
+	public String sendBName()
+	{
+		return name;
+	}
+
+	public String sendBPrice()
+	{
+		return price;
+	}
+
+	public String sendBYear()
+	{
+		String stringBYear;
+
+		stringBYear = Integer.toString(year);
+		return stringBYear;
+	}
+
+	public String sendAuthor()
+	{
+		return author;
+	}
+
+	public String sendPublisher()
+	{
+		return publisher;
+	}
+}
---
+++ src/cis2430a1/electronics.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cis2430a1;
+
+/**
+ *
+ * @author dominickhera
+ */
+public class electronics 
+{
+	private int productID;
+	private String name;
+	private String price;
+	private int year;
+	private String maker;
+
+	public electronics (int productID, String name, String price, int year, String maker)
+	{
+		this.productID = productID;
+		this.name = name;
+		this.price = price;
+		this.year = year;
+		this.maker = maker;
+	}
+
+	public int sendEProdID()
+	{
+		return productID;
+	}
+
+	public String sendStringEProdID()
+	{
+		String stringEProdID;
+
+		stringEProdID = Integer.toString(productID);
+
+		return stringEProdID;
+	}
+
+	public String sendEName()
+	{
+		return name;
+	}
+
+	public String sendEPrice()
+	{
+		return price;
+	}
+
+	public String sendEYear()
+	{
+		String stringEYear;
+
+		stringEYear = Integer.toString(year);
+		return stringEYear;
+	}
+
+	public String sendMaker()
+	{
+		return maker;
+	}
+}
